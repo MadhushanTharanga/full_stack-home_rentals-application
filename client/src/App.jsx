1fe64b9fc973938463c4b06c32449eb1acd42246
Madhushan Tharanga <110463634+MadhushanTharanga@users.noreply.github.com>
2024-05-15 21:37:20 +0530

    Add react-router-dom package and update dependencies

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,25 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import LoginPage from './pages/LoginPage'
+import RegisterPage from './pages/RegisterPage'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <div>
+        <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+        </Routes>
+        </BrowserRouter>
+      </div>
         
     </>
   )
