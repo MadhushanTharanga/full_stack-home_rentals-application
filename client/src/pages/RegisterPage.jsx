1135d73800a709fad7f2c3f0a479178ce77bc3ff
Madhushan Tharanga <110463634+MadhushanTharanga@users.noreply.github.com>
2024-05-16 18:38:12 +0530

    Update RegisterPage component and styles, and registration form inputs

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,6 +1,6 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom"
 import "../styles/Register.scss";
-import { useState } from "react";
 
 const RegisterPage = () => {
   const [formData, setFormData] = useState({
@@ -17,58 +17,88 @@ const RegisterPage = () => {
     setFormData({
       ...formData,
       [name]: value,
-      [name]: files ? files[0] : value,
+      [name]: name === "profileImage" ? files[0] : value,
     });
   };
 
-  console.log(formData);
+  const [passwordMatch, setPasswordMatch] = useState(true)
+
+  useEffect(() => {
+    setPasswordMatch(formData.password === formData.confirmPassword || formData.confirmPassword === "")
+  })
+
+  const navigate = useNavigate()
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    try {
+      const register_form = new FormData()
+
+      for (var key in formData) {
+        register_form.append(key, formData[key])
+      }
+
+      const response = await fetch("http://localhost:3001/auth/register", {
+        method: "POST",
+        body: register_form
+      })
+
+      if (response.ok) {
+        navigate("/login")
+      }
+    } catch (err) {
+      console.log("Registration failed", err.message)
+    }
+  }
 
   return (
     <div className="register">
-      <div className="register__container">
-        <form className="register__form">
+      <div className="register_content">
+        <form className="register_content_form" onSubmit={handleSubmit}>
           <input
             placeholder="First Name"
-            name="firstname"
-            defaultValue={formData.firstName}
+            name="firstName"
+            value={formData.firstName}
             onChange={handleChange}
             required
           />
           <input
             placeholder="Last Name"
-            name="lastname"
-            defaultValue={formData.lastName}
+            name="lastName"
+            value={formData.lastName}
             onChange={handleChange}
-
             required
           />
           <input
-            placeholder="Email Name"
+            placeholder="Email"
             name="email"
             type="email"
-            defaultValue={formData.email}
+            value={formData.email}
             onChange={handleChange}
-
             required
           />
           <input
             placeholder="Password"
             name="password"
-            type="password"
-            defaultValue={formData.password}
+            value={formData.password}
             onChange={handleChange}
-
+            type="password"
             required
           />
           <input
             placeholder="Confirm Password"
-            name="confirmpassword"
-            type="password"
-            defaultValue={formData.confirmPassword}
+            name="confirmPassword"
+            value={formData.confirmPassword}
             onChange={handleChange}
-
+            type="password"
             required
           />
+
+          {!passwordMatch && (
+            <p style={{ color: "red" }}>Passwords are not matched!</p>
+          )}
+
           <input
             id="image"
             type="file"
@@ -76,7 +106,6 @@ const RegisterPage = () => {
             accept="image/*"
             style={{ display: "none" }}
             onChange={handleChange}
-
             required
           />
           <label htmlFor="image">
@@ -84,18 +113,16 @@ const RegisterPage = () => {
             <p>Upload Your Photo</p>
           </label>
 
-          {
-            formData.profileImage && (
-              <img
-                src={URL.createObjectURL(formData.profileImage)}
-                alt="profile" style={{ maxWidth: "80px" }}
-              />
-            )
-          }
-          <button type="submit">Register</button>
+          {formData.profileImage && (
+            <img
+              src={URL.createObjectURL(formData.profileImage)}
+              alt="profile photo"
+              style={{ maxWidth: "80px" }}
+            />
+          )}
+          <button type="submit" disabled={!passwordMatch}>REGISTER</button>
         </form>
-        <span>Already have an account? </span>
-        <a href="/login">Log In Here</a>
+        <a href="/login">Already have an account? Log In Here</a>
       </div>
     </div>
   );
