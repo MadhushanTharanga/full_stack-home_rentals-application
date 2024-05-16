726ae3102a64ee08286c729af9e6eab58ee21ccb
Madhushan Tharanga <110463634+MadhushanTharanga@users.noreply.github.com>
2024-05-16 21:04:51 +0530

    Update LoginPage component and styles, and add login form inputs

---
+++ client/src/pages/LoginPage.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import "../styles/Login.scss"
+
+
+const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  return (
+    <div className="login">
+      <div className="login_content">
+        <form className="login_content_form" >
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) =>e.target.value}
+            required
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) =>e.target.value}
+            required
+          />
+          <button type="submit">LOG IN</button>
+        </form>
+        <a href="/register">Don't have an account? Sign In Here</a>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
